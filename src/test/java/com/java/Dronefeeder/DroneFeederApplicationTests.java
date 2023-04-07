a4247540d4b07e2d072635535e7f4c92d047b03a
Oldroge <rogeriomrjr@gmail.com>
2023-04-07 17:36:12 -0300

    first commit

---
+++ src/test/java/com/java/Dronefeeder/DroneFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.Dronefeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
