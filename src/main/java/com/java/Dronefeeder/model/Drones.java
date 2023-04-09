a67eb6db7e11bae24c71808a2466bfc677a36672
Oldroge <rogeriomrjr@gmail.com>
2023-04-09 01:50:25 -0300

    feat: relações entre Drones e latitudeLongitude

--- src/main/java/com/java/Dronefeeder/model/Drones.java
+++ src/main/java/com/java/Dronefeeder/model/Drones.java
@@ -1,10 +1,20 @@
 package com.java.Dronefeeder.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Drones {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "idLatitudeLongitude")
   private LatitudeLongitude latitudeLongitude;
 
   public Long getId() {
