51f14b7f9ab67d93be0bb912830bcf4410e700af
Oldroge <rogeriomrjr@gmail.com>
2023-04-11 18:14:47 -0300

    feat: criado pedidoController

---
+++ src/main/java/com/java/Dronefeeder/controller/PedidoController.java
@@ -0,0 +1,22 @@
+package com.java.Dronefeeder.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.java.Dronefeeder.model.Pedido;
+import com.java.Dronefeeder.service.PedidoService;
+
+@RestController
+public class PedidoController {
+  @Autowired
+  private PedidoService pedidoService;
+
+  @GetMapping("/pedidos")
+  public Iterable<Pedido> getAllPedidos() {
+    return pedidoService.getAllPedidos();
+
+  }
+
+
+
+}
