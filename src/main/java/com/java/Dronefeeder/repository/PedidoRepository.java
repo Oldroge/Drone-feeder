7650146919729cefce1045fe20b04ff48ac2213c
Oldroge <rogeriomrjr@gmail.com>
2023-04-13 00:36:37 -0300

    feat: método para pegar pedido por id de cliente

--- src/main/java/com/java/Dronefeeder/repository/PedidoRepository.java
+++ src/main/java/com/java/Dronefeeder/repository/PedidoRepository.java
@@ -1,11 +1,12 @@
 package com.java.Dronefeeder.repository;
 
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.java.Dronefeeder.model.Pedido;
 
 @Repository
 public interface PedidoRepository extends CrudRepository<Pedido, Long> {
-  List<Pedido> findAll();
+
+  Optional<Pedido> findById(Long id);
 }
