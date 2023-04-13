aa6d7dffca36badee7a6794c26bdce375f734b49
Oldroge <rogeriomrjr@gmail.com>
2023-04-13 00:40:36 -0300

    feat: criado controller que faz a requisição para o getPedidoPorId

--- src/main/java/com/java/Dronefeeder/controller/PedidoController.java
+++ src/main/java/com/java/Dronefeeder/controller/PedidoController.java
@@ -1,18 +1,23 @@
 package com.java.Dronefeeder.controller;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.java.Dronefeeder.model.Pedido;
 import com.java.Dronefeeder.service.PedidoService;
 
 @RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
   @Autowired
   private PedidoService pedidoService;
 
-  @GetMapping("/pedidos")
-  public Iterable<Pedido> getAllPedidos() {
-    return pedidoService.getAllPedidos();
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Pedido>> getPedidoPorId(@PathVariable Long id) {
+    return ResponseEntity.ok().body(pedidoService.getPedidoPorId(id));
   }
 }
